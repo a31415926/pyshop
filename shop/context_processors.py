4b4da508b34cd8c9b3bfb34333ac3553909f3f6f
John Doe <johndoe@example.com>
2021-03-21 21:29:55 +0200

    new

--- shop/context_processors.py
+++ shop/context_processors.py
@@ -3,7 +3,7 @@ from product.models import *
 def all_currency(request):
     curr = Currency.objects.all()
     select_curr = request.session.get('curr_id', 'UAH')
-    select_curr_info = Currency.objects.get(code = select_curr)
+    #select_curr_info = Currency.objects.get(code = select_curr)
     rate_select_curr = 0
     disp_select_curr = 'UAH'
     return {
