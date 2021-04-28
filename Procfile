b429a658fcdbba92d717836ef3a7d8af1cdd6fe8
John Doe <johndoe@example.com>
2021-04-28 22:20:19 +0300

    add starting celery

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: gunicorn shop.wsgi --log-file -
 release: python manage.py migrate
+worker: celery worker --shop=tasks.app
---
+++ shop/celery.py
@@ -0,0 +1,14 @@
+import os
+from celery import Celery
+ 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop.settings')
+ 
+app = Celery('shop')
+app.config_from_object('django.conf:settings')
+ 
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print('ok')
